f2745cb643ba35a519765da9796369baae76c300
Abdullah Al Mamun <mamun12121999@gmail.com>
2022-10-08 19:14:53 +0600

    initial commit

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import banner from '../../images/image/banner.png'
+
+const Banner = () => {
+    return (
+        <div >
+            
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/components/Banner/SubscribeOuts.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SubscribeOuts = () => {
+  return <div></div>;
+};
+
+export default SubscribeOuts;
