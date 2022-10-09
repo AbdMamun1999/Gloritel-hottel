e791d2cb2c92cdfb472b6543960a8bd8dbf9da30
Abdullah Al Mamun <mamun12121999@gmail.com>
2022-10-09 15:46:05 +0600

    style subscribe section

--- src/components/Banner/SubscribeSeciton.js
+++ src/components/Banner/SubscribeSeciton.js
@@ -2,15 +2,24 @@ import React from "react";
 
 const SubscribeSeciton = () => {
   return (
-    <div className="flex flex-col gap-y-10  items-center">
+    <div className="flex flex-col gap-y-10  items-center my-20">
       <div>
-        <h1>Subscribe to Our Newsletter</h1>
-        <p>Don’t miss this opportunity. Subscribe now</p>
+        <h1 className="text-[60px] text-center">Subscribe to Our Newsletter</h1>
+        <p className="text-[20px] text-center">
+          Don’t miss this opportunity. Subscribe now
+        </p>
       </div>
       <div className="">
         <form>
-          <input type="" name="" value="" className="border bg-[#EDEDED]" />
-          <input type="submit" name="" value="SUBSCRIBE" />
+          <input
+            type="email"
+            className="border bg-[#EDEDED] lg:w-[550px] px-6 py-3 outline-none rounded-l"
+          />
+          <input
+            type="submit"
+            value="SUBSCRIBE"
+            className="bg-[#FDC463] px-3 py-3 rounded-r"
+          />
         </form>
       </div>
     </div>
